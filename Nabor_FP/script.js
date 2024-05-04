c9721e5c9eb7fd2deb3314ed2ed1750430c2c3c7
NiruNeilNix <157327669+NiruNeilNix@users.noreply.github.com>
2024-05-04 23:53:11 +0800

    Add files via upload

---
+++ Nabor_FP/script.js
@@ -0,0 +1,25 @@
+let displayValue = '';
+
+function addToDisplay(value) {
+    displayValue += value;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
